b78faa7e7b39a7da85cb408d935c8a467e002432
eagle23111 <stasapohta@yandex.ru>
2026-02-05 22:53:52 +0300

    update

--- niriDotfiles/home.nix
+++ niriDotfiles/home.nix
@@ -2,32 +2,7 @@
 
 {
   imports = [
-    inputs.dms.homeModules.dank-material-shell
-    inputs.dms.homeModules.niri
-    inputs.niri.homeModules.niri
+    ./homeModules/dms.nix
+    ./homemodules/niri.nix
   ];
-
-  programs.dank-material-shell = {
-    enable = true;
-    dgop.package = pkgs.unstable.dgop;
-    # Core features
-    # niri integration
-    niri = {
-      enableSpawn = false;   # DMS will start niri automatically
-      enableKeybinds = false;  # Prefer config includes (more flexible)
-      includes = {
-        enable = true;
-        override = true;
-        originalFileName = "hm";
-        filesToInclude = [ "binds" "layout" "outputs" ];
-      };
-    };
-
-    settings = {
-      theme = "dark";
-      dynamicTheming = true;
-    };
-  };
-
-  home.packages = [ pkgs.unstable.dgop ];
 }
